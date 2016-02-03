32ed84ca643a9571e0b859b9895513ec7e38cf6d
Gregory Pons <ponsgregory@gmail.co>
2016-02-03 14:17:59 +0100

    add/remove user

--- src/BirdOffice/UserBundle/Controller/AjaxController.php
+++ src/BirdOffice/UserBundle/Controller/AjaxController.php
@@ -2,13 +2,78 @@
 
 namespace BirdOffice\UserBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
 
 
 class AjaxController extends Controller
 {
+    /**
+     * @Route("/ajaxCall", name="addPartnerAjax", options={"expose"=true} )
+     *
+     */
+    public function AddPartnerAjaxAction(Request $request)
+    {
+        $managerId = $request->get('managedBy');
+
+        $em = $this->getDoctrine();
+        // $manager = $em->getRepository('UserBundle:User')->find($managerId);
+
+
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->get('fos_user.registration.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->get('event_dispatcher');
+
+        $user = $userManager->createUser();
+        $user->setEnabled(true);
+
+        $user->setEmail($request->get('email'));
+        $user->setCivility($request->get('civility'));
+        $user->setName($request->get('name'));
+        $user->setUsername($request->get('username'));
+        $user->setPlainPassword($request->get('plainPassword'));
+       // $user->setManagedBy();
+
+        $userManager->updateUser($user);
+    }
+
+    /**
+     * @Route("/deleteUser/{id}", name="delete_user", options={"expose"=true} )
+     *
+     */
+    public function DeleteUserAjaxAction(Request $request)
+    {
+        $em = $this->getDoctrine();
+
+        $userId = $request->get('id');
+
+        $user = $em->getRepository('UserBundle:User')->find($userId);
+
+        $userManager = $this->get('fos_user.user_manager');
+
+        $userManager->deleteUser($user);
+
+        $managers = $em->getRepository('UserBundle:User')->findByRole('ROLE_SUPER_ADMIN');
+
+        return $this->render('UserBundle:Default:index.html.twig',
+            array(
+                'managers' => $managers
+            )
+        );    }
+
 
 }
--- src/BirdOffice/UserBundle/Controller/RegistrationController.php
+++ src/BirdOffice/UserBundle/Controller/RegistrationController.php
@@ -16,36 +16,8 @@ class RegistrationController extends BaseController
 
     public function RegisterAction(Request $request)
     {
-
         $formFactory = $this->container->get('fos_user.registration.form.factory');
         $form = $formFactory->createForm();
-        /*
-        $formHandler = $this->container->get('fos_user.registration.form.handler');
-        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
-
-        $process = $formHandler->process($confirmationEnabled);
-        if ($process) {
-            $user = $form->getData();
-
-
-            $this->container->get('logger')->info(
-                sprintf('New user registration: %s', $user)
-            );
-
-            if ($confirmationEnabled) {
-                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
-                $route = 'fos_user_registration_check_email';
-            } else {
-                $this->authenticateUser($user);
-                $route = 'fos_user_registration_confirmed';
-            }
-
-            $this->setFlash('fos_user_success', 'registration.flash.user_created');
-            $url = $this->container->get('router')->generate($route);
-
-            return new RedirectResponse($url);
-        }
-        */
 
         return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.twig', array(
             'form' => $form->createView()
@@ -53,12 +25,4 @@ class RegistrationController extends BaseController
     }
 
 
-    /**
-     * @Route("/ajaxCall", name="addPartnerAjax", options={"expose"=true} )
-     *
-     */
-    public function AddPartnerAjaxAction()
-    {
-die('ok');
-    }
 }
